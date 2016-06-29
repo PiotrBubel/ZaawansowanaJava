60352ca03eb76e6c3e699e258f6fcb15371714d4
Sebastian Kaźmierczak <SebastianKazmierczak1@gmail.com>
2016-06-29 22:59:37 +0200

    frontend -added new Game window

---
+++ src/main/java/frontend/newGame/NewGameWindow.java
@@ -0,0 +1,117 @@
+package frontend.newGame;
+
+import frontend.interfaces.NewGame;
+import npuzzle.Board;
+import npuzzle.utils.BoardUtils;
+
+public class NewGameWindow extends javax.swing.JFrame {
+
+    private NewGame gameWindow;
+
+    public NewGameWindow(NewGame newGame) {
+        gameWindow = newGame;
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        startButton = new javax.swing.JButton();
+        rowsSpinner = new javax.swing.JSpinner();
+        columnsSpinner = new javax.swing.JSpinner();
+        movesSpinner = new javax.swing.JSpinner();
+        rowsLabel = new javax.swing.JLabel();
+        columnsLabel = new javax.swing.JLabel();
+        movesLabel = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        startButton.setText("Start !");
+        startButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                startButtonActionPerformed(evt);
+            }
+        });
+
+        rowsSpinner.setModel(new javax.swing.SpinnerNumberModel(4, 1, 9, 1));
+        rowsSpinner.setToolTipText("");
+
+        columnsSpinner.setModel(new javax.swing.SpinnerNumberModel(4, 1, 9, 1));
+        columnsSpinner.setToolTipText("");
+
+        movesSpinner.setModel(new javax.swing.SpinnerNumberModel(Integer.valueOf(20), Integer.valueOf(5), null, Integer.valueOf(1)));
+        movesSpinner.setToolTipText("");
+
+        rowsLabel.setText("Rows");
+
+        columnsLabel.setText("Columns");
+
+        movesLabel.setText("Moves");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(startButton)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addComponent(rowsLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(rowsSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addComponent(columnsLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(columnsSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addComponent(movesLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(movesSpinner)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rowsSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(rowsLabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(columnsSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(columnsLabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(movesSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(movesLabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+                .addComponent(startButton)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void startButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_startButtonActionPerformed
+        // TODO add your handling code here:
+        int rows = (int) rowsSpinner.getValue();
+        int columns = (int) columnsSpinner.getValue();
+        int moves = (int) movesSpinner.getValue();
+        Board newBoard = BoardUtils.randomizeBoard(rows, columns, moves);
+        gameWindow.setNewGame(newBoard);
+        this.dispose();
+        
+    }//GEN-LAST:event_startButtonActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel columnsLabel;
+    private javax.swing.JSpinner columnsSpinner;
+    private javax.swing.JLabel movesLabel;
+    private javax.swing.JSpinner movesSpinner;
+    private javax.swing.JLabel rowsLabel;
+    private javax.swing.JSpinner rowsSpinner;
+    private javax.swing.JButton startButton;
+    // End of variables declaration//GEN-END:variables
+}
