3720c088c610ded7dc1e1ad70f3e63154c444680
Sebastian Kaźmierczak <SebastianKazmierczak1@gmail.com>
2016-07-05 23:11:41 +0200

    deleted unused fields in AnimatorBoardController

--- src/main/java/frontend/utils/AnimatorBoardController.java
+++ src/main/java/frontend/utils/AnimatorBoardController.java
@@ -8,19 +8,17 @@ import npuzzle.Board;
 public class AnimatorBoardController extends DefaultBoardController {
 
     private String pathToWin;
-    // private Board startingBoard;
 
     public AnimatorBoardController(JPanel drawingPanel, Board board, String pathToWin) {
         super(drawingPanel, board);
         this.pathToWin = pathToWin;
-        //   startingBoard = new Board(board);
     }
 
     @Override
     public void move(int numberOfMove) {
         try {
             board = board.move(pathToWin.charAt(numberOfMove));
-            
+
         } catch (BoardWithoutZeroException ex) {
             System.out.println(ex.getMessage());
         }
